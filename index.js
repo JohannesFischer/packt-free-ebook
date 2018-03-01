cca2416d7f5a20ee9245adb3cee5533d83360eb8
Johannes Fischer <johannes.fischer@rakuten.com>
2018-03-01 17:45:21 +0900

    async all the things, doubts that ...

--- index.js
+++ index.js
@@ -1,38 +1,102 @@
+const request = require('request-promise');
+const cheerio = require('cheerio');
+const inquirer = require('inquirer');
 const webdriver = require('selenium-webdriver');
 const { By, until } = webdriver;
 const firefox = require('selenium-webdriver/firefox');
-// console.log(firefox.Channel)
 
-// process.exit()
+// For screenshots only
+const util = require('util');
+const fs = require('fs');
+const writeFile = util.promisify(fs.writeFile);
+
 const binary = new firefox.Binary(firefox.Channel.Release);
-binary.addArguments('-headless');
+// binary.addArguments('-headless');
+// binary.addArguments('--window-size=1100,600');
 
 const driver = new webdriver.Builder()
-    // .withCapabilities(capabilities)
     .forBrowser('firefox')
     .setFirefoxOptions(new firefox.Options().setBinary(binary))
     .build();
 
 const freeBookUrl = 'https://www.packtpub.com//packt/offers/free-learning';
 
-// Open page
-driver.get(freeBookUrl);
+// Fetch book details
 
-// Log Book title
-
-driver.findElement(By.css('#deal-of-the-day h2')).then(title => {
-  title.getText().then(text => {
-    console.log(text)
+async function main() {
+  const $ = await request({
+    uri: freeBookUrl,
+    transform: function (body) {
+      return cheerio.load(body);
+    }
   });
-});
+
+  const bookTitle = $('#deal-of-the-day h2').text().trim();
+
+  console.log('The current free ebook is: ', bookTitle);
+
+  // Prompt to claim the book
+
+  const claimInquiry = await inquirer.prompt({
+    name: 'claim',
+    message: 'Interested?',
+    type: 'confirm'
+  });
+
+  if (!claimInquiry.claim) {
+    console.log('K, thanks bye.');
+    process.exit();
+  }
+
+  // Load Packt website
+  console.log('Preparing to login...');
+
+  await driver.get(freeBookUrl);
+
+  await driver.sleep(1000);
+
+  await driver.executeScript("document.querySelector('#account-bar-login-register .login-popup').scrollIntoView(true);");
+
+  const data = await driver.takeScreenshot();
+  await writeFile('screenshot.png', data, 'base64');
+
+  const loginToggle = await driver.findElement(By.css('#account-bar-login-register .login-popup'));
+
+  await loginToggle.click;
+
+  await driver.sleep(5000);
+  await driver.quit();
+  //       driver.get(freeBookUrl).then(() => {
+  //         driver.takeScreenshot();
+  //         const questions = [
+  //           {
+  //             name: 'email',
+  //             message: 'Email address',
+  //             type: 'input'
+  //           },
+  //           {
+  //             name: 'password',
+  //             message: 'Password',
+  //             type: 'password'
+  //           }
+  //         ];
+
+  //         inquirer.prompt(questions).then(answers => {
+  //           console.log(answers);
+
+  //           driver.findElement(By.css('#account-bar-login-register .login-popup')).then(login => {
+  //             login.click();
+  //           });
+  //         });
+  //       });
+  //     });
+  //   }
+  // });
+}
+
+main();
 
 // What's next?
-
-// Prompt to download
-
-// if 'nay': end process
-
-// if 'yay': Login
 
 // Login and follow redirect to free ebook page
 
@@ -44,4 +108,4 @@ driver.findElement(By.css('#deal-of-the-day h2')).then(title => {
 
 // Bonus point two: check for duplicates for automated use
 
-driver.quit();
+// driver.quit();
